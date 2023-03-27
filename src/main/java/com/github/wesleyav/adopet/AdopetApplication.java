64b967a024fe7a804676b3eeef443073f9d01a10
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2023-03-27 11:06:36 -0300

    Configura Springdoc Open API

--- src/main/java/com/github/wesleyav/adopet/AdopetApplication.java
+++ src/main/java/com/github/wesleyav/adopet/AdopetApplication.java
@@ -1,13 +1,28 @@
 package com.github.wesleyav.adopet;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class AdopetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdopetApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI().info(new Info().title("Adopet").version(appVersion)
+				.description(
+						"Adopet API.")
+				.termsOfService("http://swagger.io/terms/")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
