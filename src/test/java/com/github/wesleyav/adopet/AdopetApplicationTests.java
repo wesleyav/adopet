fa0d76302316e187040e1111c818feeaa432d323
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2023-03-27 10:29:11 -0300

    Adiciona estrutura do projeto

---
+++ src/test/java/com/github/wesleyav/adopet/AdopetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.adopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
