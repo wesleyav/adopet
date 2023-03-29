d50fdeb7e725496bb60445fc06dec2470d29ff5a
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2023-03-28 22:32:23 -0300

    Adiciona tratamentos de exceções

---
+++ src/main/java/com/github/wesleyav/adopet/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.github.wesleyav.adopet.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.github.wesleyav.adopet.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+
+		StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(standardError);
+	}
+
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		String mensagem = "";
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+			mensagem = mensagem + error.getDefaultMessage() + "\n";
+		}
+
+		return handleExceptionInternal(ex, mensagem, headers, status, request);
+	}
+
+}
---
+++ src/main/java/com/github/wesleyav/adopet/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.github.wesleyav.adopet.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
