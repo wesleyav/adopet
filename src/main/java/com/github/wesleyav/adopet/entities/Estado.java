a84a1e0de1ae37554e76690ea8555dbf239fb10b
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2023-04-04 17:02:23 -0300

    Adiciona classe Estado

---
+++ src/main/java/com/github/wesleyav/adopet/entities/Estado.java
@@ -0,0 +1,30 @@
+package com.github.wesleyav.adopet.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "estado")
+public class Estado implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String sigla;
+
+}
