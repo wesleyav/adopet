32aa4cc7d70d55cb0d5f84859671e276f8b349fa
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2023-04-04 16:59:27 -0300

    Adiciona classe AbrigoService

---
+++ src/main/java/com/github/wesleyav/adopet/services/AbrigoService.java
@@ -0,0 +1,97 @@
+package com.github.wesleyav.adopet.services;
+
+import java.time.Instant;
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.github.wesleyav.adopet.entities.Abrigo;
+import com.github.wesleyav.adopet.entities.Cidade;
+import com.github.wesleyav.adopet.entities.Endereco;
+import com.github.wesleyav.adopet.entities.Estado;
+import com.github.wesleyav.adopet.repositories.AbrigoRepository;
+import com.github.wesleyav.adopet.repositories.EnderecoRepository;
+import com.github.wesleyav.adopet.services.exceptions.ResourceEmptyException;
+import com.github.wesleyav.adopet.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class AbrigoService {
+
+	private AbrigoRepository abrigoRepository;
+	private EnderecoRepository enderecoRepository;
+
+	public AbrigoService(AbrigoRepository abrigoRepository, EnderecoRepository enderecoRepository) {
+		this.abrigoRepository = abrigoRepository;
+		this.enderecoRepository = enderecoRepository;
+	}
+
+	public List<Abrigo> findAll() {
+		List<Abrigo> abrigos = abrigoRepository.findAll();
+
+		if (abrigos.isEmpty()) {
+			throw new ResourceEmptyException("Nenhum registro encontrado.");
+		}
+
+		return abrigos;
+	}
+
+	public Abrigo findById(Integer id) {
+		return abrigoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	@Transactional
+	public Abrigo save(Abrigo abrigo) {
+		Endereco endereco = abrigo.getEndereco();
+		enderecoRepository.save(endereco);
+		abrigo.setEndereco(endereco);
+		abrigo.setCreatedAt(Instant.now());
+		return abrigoRepository.save(abrigo);
+	}
+
+	public void deleteById(Integer id) {
+		try {
+			abrigoRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	@Transactional
+	public Abrigo update(Integer id, Abrigo obj) {
+		try {
+			Abrigo abrigoExistente = abrigoRepository.getReferenceById(id);
+
+			if (abrigoExistente == null) {
+				throw new ResourceEmptyException("Nenhum registro encontrado.");
+			}
+
+			Endereco enderecoExistente = abrigoExistente.getEndereco();
+			Endereco enderecoAtualizado = obj.getEndereco();
+			enderecoExistente.setLogradouro(enderecoAtualizado.getLogradouro());
+			enderecoExistente.setNumero(enderecoAtualizado.getNumero());
+			enderecoExistente.setCep(enderecoAtualizado.getCep());
+
+			Cidade cidadeExistente = abrigoExistente.getEndereco().getBairro().getCidade();
+			Cidade cidadeAtualizada = obj.getEndereco().getBairro().getCidade();
+			cidadeExistente.setNome(cidadeAtualizada.getNome());
+
+			Estado estadoExistente = abrigoExistente.getEndereco().getBairro().getCidade().getEstado();
+			Estado estadoAtualizado = obj.getEndereco().getBairro().getCidade().getEstado();
+			estadoExistente.setSigla(estadoAtualizado.getSigla());
+
+			abrigoExistente.setNome(obj.getNome());
+			abrigoExistente.setUpdatedAt(Instant.now());
+
+			return abrigoRepository.save(abrigoExistente);
+
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
+	}
+
+}
