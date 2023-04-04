fd71b43529ba7d357eefda31e4059e10c2c9a0ea
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2023-04-04 16:59:59 -0300

    Adiciona classe Endereco

---
+++ src/main/java/com/github/wesleyav/adopet/entities/Endereco.java
@@ -0,0 +1,41 @@
+package com.github.wesleyav.adopet.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "endereco")
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String logradouro;
+
+	private String numero;
+
+	private String cep;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "bairro", referencedColumnName = "id")
+	private Bairro bairro;
+
+}
