8764c3d5d9b93bbc4043c7adb3f3d1110c9d8c7c
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2023-04-04 17:01:16 -0300

    Adiciona classe Bairro

---
+++ src/main/java/com/github/wesleyav/adopet/entities/Bairro.java
@@ -0,0 +1,37 @@
+package com.github.wesleyav.adopet.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "bairro")
+public class Bairro implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String nome;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "cidade", referencedColumnName = "id")
+	private Cidade cidade;
+
+}
