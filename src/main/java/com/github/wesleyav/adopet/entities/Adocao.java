77f63990f282a41bf195c25745426fc370f5b739
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2023-04-05 16:30:35 -0300

    Adiciona classe Adocao

---
+++ src/main/java/com/github/wesleyav/adopet/entities/Adocao.java
@@ -0,0 +1,38 @@
+package com.github.wesleyav.adopet.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "adocao")
+public class Adocao implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	private UUID id;
+
+	private Integer animalId;
+
+	private Integer tutorId;
+
+	private Instant dataAdocao;
+
+}
