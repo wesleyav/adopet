96251fd371083a01dff899cd19a387cf377e6760
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2023-04-12 18:17:21 -0300

    Refatora anotações de Lombok na classe Estado + testes unitários

--- src/main/java/com/github/wesleyav/adopet/entities/Estado.java
+++ src/main/java/com/github/wesleyav/adopet/entities/Estado.java
@@ -1,6 +1,7 @@
 package com.github.wesleyav.adopet.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,12 +10,16 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString(of = { "id", "sigla" })
 @Entity
 @Table(name = "estado")
 public class Estado implements Serializable {
@@ -26,5 +31,22 @@ public class Estado implements Serializable {
 	private Integer id;
 
 	private String sigla;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estado other = (Estado) obj;
+		return Objects.equals(id, other.id);
+	}
 
 }
--- src/test/java/com/github/wesleyav/adopet/entities/EstadoTest.java
+++ src/test/java/com/github/wesleyav/adopet/entities/EstadoTest.java
@@ -1,6 +1,9 @@
 package com.github.wesleyav.adopet.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +21,64 @@ public class EstadoTest {
 		Estado estado = new Estado();
 		estado.setSigla("SP");
 		assertEquals("SP", estado.getSigla());
+	}
+
+	@Test
+	public void testToString() {
+		Estado estado = new Estado(1, "SP");
+		String expected = "Estado(id=" + estado.getId() + ", sigla=" + estado.getSigla() + ")";
+		assertEquals(expected, estado.toString());
+	}
+
+	@Test
+	public void testHashCode() {
+		Estado estado1 = new Estado(1, "SP");
+		Estado estado2 = new Estado(1, "SP");
+		Estado estado3 = new Estado(2, "RN");
+
+		assertEquals(estado1.hashCode(), estado2.hashCode());
+		assertNotEquals(estado1.hashCode(), estado3.hashCode());
+	}
+
+	@Test
+	public void testEquals() {
+		Estado estado1 = new Estado(1, "SP");
+		Estado estado2 = new Estado(1, "SP");
+		Estado estado3 = new Estado(2, "RN");
+
+		assertTrue(estado1.equals(estado2));
+		assertFalse(estado1.equals(estado3));
+	}
+
+	@Test
+	public void testEqualsNull() {
+		Estado estado = new Estado(1, "SP");
+		assertFalse(estado.equals(null));
+	}
+
+	@Test
+	public void testEqualsDifferentClass() {
+		Estado estado = new Estado(1, "SP");
+		assertFalse(estado.equals("SP"));
+	}
+
+	@Test
+	public void testEqualsSameObject() {
+		Estado estado = new Estado(1, "SP");
+		assertTrue(estado.equals(estado));
+	}
+
+	@Test
+	public void testEqualsDifferentObjectSameId() {
+		Estado estado1 = new Estado(1, "SP");
+		Estado estado2 = new Estado(1, "RJ");
+		assertTrue(estado1.equals(estado2));
+	}
+
+	@Test
+	public void testEqualsDifferentObjectDifferentId() {
+		Estado estado1 = new Estado(1, "SP");
+		Estado estado2 = new Estado(2, "SP");
+		assertFalse(estado1.equals(estado2));
 	}
 }
